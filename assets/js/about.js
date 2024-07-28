d99745997add14932be83a14c9a09c3436ad5209
Savindu Fernando <144669409+savindufernando@users.noreply.github.com>
2024-07-28 11:49:45 +0530

    Add files via upload

---
+++ assets/js/about.js
@@ -0,0 +1,7 @@
+function showContactForm() {
+    document.getElementById('contact-form').style.display = 'block';
+}
+
+function hideContactForm() {
+    document.getElementById('contact-form').style.display = 'none';
+}
